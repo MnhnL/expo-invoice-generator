b28837620fc020b963348603488a9516a4b828da
Raffael Mancini <raffael.mancini@mnhn.lu>
2024-11-05 10:04:16 +0100

    Read xlsx now

--- expo-invoice-generator.py
+++ expo-invoice-generator.py
@@ -1,7 +1,12 @@
 import csv
+import os.path
+from collections import defaultdict
+import datetime
+
+from openpyxl import load_workbook
+
 from fpdf import FPDF, XPos, YPos, Align
 from fpdf.enums import MethodReturnValue, WrapMode
-from collections import defaultdict
 
 FONT_SZ_HEADER = 14
 FONT_SZ_FOOTER = 7
@@ -11,7 +16,11 @@ FONT_SZ_TABLE_ROW = 8
 
 FONT_SZ_NORMAL = 10
 
-FONT_PATH = '/usr/share/fonts/truetype/dejavu'
+FONT_PATH = "/usr/share/fonts/truetype/dejavu"
+FONT_NAME = "DejaVuSans"
+FONT_FILE_REGULAR = "DejaVuSans.ttf"
+FONT_FILE_OBLIQUE = "DejaVuSans-Oblique.ttf"
+FONT_FILE_BOLD = "DejaVuSans-Bold.ttf"
 
 ROW_HEIGHT_HEADER = 8
 ROW_HEIGHT_ROW = 6
@@ -32,14 +41,14 @@ class PDF(FPDF):
         self.local_page_no = self.page_no()
     
     def header(self):
-        self.set_font("DejaVuSans", "B", FONT_SZ_HEADER)
+        self.set_font(FONT_NAME, "B", FONT_SZ_HEADER)
         self.cell(0, 10, "Relevé des visites organisées par le MNHN", align=Align.C, border=1, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
         self.ln(8)
 
     def footer(self):
         # Go to 1.5 cm from bottom
         self.set_y(-15)
-        self.set_font("DejaVuSans", "", FONT_SZ_FOOTER)
+        self.set_font(FONT_NAME, "", FONT_SZ_FOOTER)
         self.cell(0, 10, f"Page {self.page_no() - self.local_page_no + 1}", align=Align.R, new_x=XPos.RIGHT, new_y=YPos.TOP)
 
     def row(self, height, cells, border="B",
@@ -57,7 +66,7 @@ class PDF(FPDF):
                 new_x = XPos.LMARGIN
                 new_y = YPos.NEXT
 
-            self.set_font("DejaVuSans", weight, FONT_SZ_TABLE_ROW)
+            self.set_font(FONT_NAME, weight, FONT_SZ_TABLE_ROW)
             cell_col_lines.append(
                 self.multi_cell(width, height, text, border=border, align=a,
                                 new_x=new_x, new_y=new_y, wrapmode=WrapMode.CHAR,
@@ -75,7 +84,7 @@ class PDF(FPDF):
                 new_x = XPos.LMARGIN
                 new_y = YPos.NEXT
 
-            self.set_font("DejaVuSans", weight, FONT_SZ_TABLE_ROW)
+            self.set_font(FONT_NAME, weight, FONT_SZ_TABLE_ROW)
             empty_lines = max_lines - len(cell_lines)
             text = "".join(cell_lines + ["\n " * empty_lines])
 
@@ -83,7 +92,7 @@ class PDF(FPDF):
                             new_x=new_x, new_y=new_y, wrapmode=WrapMode.CHAR)
 
     def add_commune_data(self, commune, events):
-        self.set_font("DejaVuSans", "B", FONT_SZ_TABLE_HEADER)
+        self.set_font(FONT_NAME, "B", FONT_SZ_TABLE_HEADER)
         self.row(ROW_HEIGHT_HEADER,
                  ["Date / # Activité", "Responsable", "Nom", "Prix"],
                  weights=["I", "I", "I", "I"],
@@ -91,7 +100,7 @@ class PDF(FPDF):
 
         total_sum = 0
 
-        self.set_font("DejaVuSans", "", FONT_SZ_TABLE_ROW)
+        self.set_font(FONT_NAME, "", FONT_SZ_TABLE_ROW)
         for entry in events:
             total_sum += entry['price']
 
@@ -106,49 +115,75 @@ class PDF(FPDF):
                      weights=["", "I", "", ""])
 
         # Total sum for the commune
-        self.set_font("DejaVuSans", "B", FONT_SZ_TABLE_ROW)
+        self.set_font(FONT_NAME, "B", FONT_SZ_TABLE_ROW)
         self.row(ROW_HEIGHT_ROW, ["", "", "Total", format_price(total_sum)],
                  weights=["", "", "B", "B"],
                  aligns=["L", "L", "L", "R"])
 
-def generate_reports(csv_file_path):
+def generate_reports(file_path):
     data = defaultdict(list)
+    extension = file_path.split(".")[1]
+    
+    get_col = None # Extracts value from row and col name
+    rows = None # Iterates rows
+    
+    if extension == "csv":
+        csvfile = open(file_path, newline='')
 
-    with open(csv_file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            activity = row["Offer\nName"]
-            customer_invoice_address_name = row["Customer\nInvoice address name"]
-            data[customer_invoice_address_name].append({
-                'cia_name': row["Customer\nInvoice address name"],
-                'cia_street': row["Customer\nInvoice address street"],
-                'cia_zip': row["Customer\nInvoice address postal code"],
-                'cia_city': row["Customer\nInvoice address city"],
-                'activity': activity,
-                'responsable': row["Booker\nFull name"],
-                'customer_name': row["Customer\nName"],
-                'booking_number': row["Booking\nNumber"],
-                'datetime': row["Offer\nEnd date & time"],
-                'price': float(row["Reservation\nPrice"])
-            })
+        def get_col(row, col_name):
+            return row[col_name]
+        rows = csv.DictReader(csvfile)
+
+    elif extension == 'xlsx':
+        wb = load_workbook(filename=file_path)
+        sh = wb.active
+        max_row = sh.max_row
+
+        row0 = ([r for r in sh.iter_rows(min_row=1, max_row=1)])[0]
+        cols = {c.value: c.column for c in row0}
+
+        def get_col(row, col_name):
+            return row[cols[col_name]-1].value
+        rows = sh.iter_rows(min_row=2, max_row=max_row)
+
+    else:
+        raise Exception("Cant read this file type. Try csv or xlsx!")
+
+    # Actually read
+    for row in rows:
+        activity = get_col(row, "Offer\nName")
+        customer_invoice_address_name = get_col(row, "Customer\nInvoice address name")
+        data[customer_invoice_address_name].append({
+            'cia_name': get_col(row, "Customer\nInvoice address name"),
+            'cia_street': get_col(row, "Customer\nInvoice address street"),
+            'cia_zip': get_col(row, "Customer\nInvoice address postal code"),
+            'cia_city': get_col(row, "Customer\nInvoice address city"),
+            'activity': activity,
+            'responsable': get_col(row, "Booker\nFull name"),
+            'customer_name': get_col(row, "Customer\nName"),
+            'booking_number': get_col(row, "Booking\nNumber"),
+            'datetime': get_col(row, "Offer\nEnd date & time"),
+            'price': float(get_col(row, "Reservation\nPrice"))
+        })
+
+    csvfile.close()
 
     pdf = PDF()
     
     pdf.set_margins(16, 20, 16) # L T R 
     for customer_invoice_address_name, activities in data.items():
-        
-        pdf.add_font('DejaVuSans', '', f'{FONT_PATH}/DejaVuSans.ttf')
-        pdf.add_font('DejaVuSans', 'B', f'{FONT_PATH}/DejaVuSans-Bold.ttf')
-        pdf.add_font('DejaVuSans', 'I', f'{FONT_PATH}/DejaVuSans-Oblique.ttf')
+        pdf.add_font(FONT_NAME, '', os.path.join(FONT_PATH, FONT_FILE_REGULAR))
+        pdf.add_font(FONT_NAME, 'B', os.path.join(FONT_PATH, FONT_FILE_BOLD))
+        pdf.add_font(FONT_NAME, 'I', os.path.join(FONT_PATH, FONT_FILE_OBLIQUE))
         pdf.add_page()
         pdf.new_document()
 
         # Initial content
-        pdf.set_font("DejaVuSans", "B", FONT_SZ_NORMAL)
+        pdf.set_font(FONT_NAME, 'B', FONT_SZ_NORMAL)
         pdf.multi_cell(0, 5, f"Adresse de facturation pour la commune {customer_invoice_address_name} et les visites ci-dessous:", 0, 'L')
         pdf.ln(2)
 
-        pdf.set_font("DejaVuSans", "", FONT_SZ_NORMAL)
+        pdf.set_font(FONT_NAME, '', FONT_SZ_NORMAL)
         pdf.set_x(pdf.get_x() + 10)
 
         a0 = activities[0]
@@ -159,9 +194,10 @@ def generate_reports(csv_file_path):
         # Adding activity data
         pdf.add_commune_data(customer_invoice_address_name, activities)
 
-    filename_suffix = customer_invoice_address_name.replace(' ', '-').replace("'", "")
-    pdf.output(f"facture_{filename_suffix}.pdf")
-    print(f"Generated PDF for {customer_invoice_address_name}")
+    suffix = datetime.datetime.now().isoformat()
+    filename = f"facture_{suffix}.pdf"
+    pdf.output(filename)
+    print(f"Generated PDF {filename}")
 
 if __name__ == '__main__':
     import sys
